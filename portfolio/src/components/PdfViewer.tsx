b49e6c88b0a428e4fea2d682fe4d91a5d4d6ee62
david87chdz <david87chdz@gmail.com>
2024-10-16 23:02:44 +0200

    Changes the cv

--- portfolio/src/components/PdfViewer.tsx
+++ portfolio/src/components/PdfViewer.tsx
@@ -1,15 +1,28 @@
 import { useState, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import '../styles/PDFViewer.css'; 
 
 pdfjs.GlobalWorkerOptions.workerSrc = `/pdfjs/pdf.worker.min.mjs`;
 
 export function PDFViewer({ fileUrl }: { fileUrl: string }) {
-  const [, setNumPages] = useState(1);
-  const [pageNumber, ] = useState(1);
+  const [numPages, setNumPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
   const containerRef = useRef<HTMLDivElement>(null);
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
+  };
+
+  const goToNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const goToPrevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
   };
 
   return (
@@ -20,9 +33,26 @@ export function PDFViewer({ fileUrl }: { fileUrl: string }) {
       >
         <Page
           pageNumber={pageNumber}
-          scale={1.2} 
+          scale={1.2}
+          renderTextLayer={false} // Desactivar el renderizado del texto
+          renderAnnotationLayer={false} // Desactivar el renderizado de anotaciones
         />
       </Document>
+
+      {/* Botones de navegación */}
+      <div className="pdf-navigation">
+        {pageNumber > 1 && (
+          <button onClick={goToPrevPage}>
+            Anterior
+          </button>
+        )}
+        {pageNumber < numPages && (
+          <button onClick={goToNextPage}>
+            Siguiente
+          </button>
+        )}
+        <p style={{ color: '#333' }}>Página {pageNumber} de {numPages}</p> {/* Texto más oscuro */}
+      </div>
     </div>
   );
 }
